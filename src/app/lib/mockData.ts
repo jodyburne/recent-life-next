faa94ed84aac7a6be2015835d31585a77ebc9a1c
jodyburne <j__richardson@live.co.uk>
2024-12-03 21:57:37 +0000

    chore: project set up

---
+++ src/app/lib/mockData.ts
@@ -0,0 +1,29 @@
+type MockData = {
+  title: string;
+  description?: string;
+  src: string;
+}[];
+
+const korea: MockData = [
+  {
+    title: "Salt bread",
+    description: "",
+    src: "https://images.food52.com/zytoqj3sW8KJhhcEijUS65OTxkk=/1200x1200/683ae8f7-5fad-4d1f-830d-243f3e52f87a--DSC02382.jpg",
+  },
+];
+
+const japan: MockData = [
+  {
+    title: "Salt bread",
+    description: "",
+    src: "https://images.food52.com/zytoqj3sW8KJhhcEijUS65OTxkk=/1200x1200/683ae8f7-5fad-4d1f-830d-243f3e52f87a--DSC02382.jpg",
+  },
+];
+
+const teapots: MockData = [
+  {
+    title: "Salt bread",
+    description: "",
+    src: "https://images.food52.com/zytoqj3sW8KJhhcEijUS65OTxkk=/1200x1200/683ae8f7-5fad-4d1f-830d-243f3e52f87a--DSC02382.jpg",
+  },
+];
